6035674297b69fe9ee56c0ccbc4792b6a1e759b4
halkimrajan <halkimrajan@gmail.com>
2023-09-26 20:51:00 +0800

    frontend separation of concern for taskView

--- frontend/src/components/FiterableTaskTable.tsx
+++ frontend/src/components/FiterableTaskTable.tsx
@@ -1,24 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import Chip from "@mui/material/Chip";
-
-import DeleteOutline from "@mui/icons-material/DeleteOutline";
-
-import { Priority, Task } from "../types/tasks";
-import {
-  completeTask,
-  getTasks,
-  uncompleteTask,
-  deleteTask,
-} from "../api/tasks";
+import { Task } from "../types/tasks";
+import { getTasks } from "../api/tasks";
 import {
   FilterTypes,
   SortTypes,
@@ -27,7 +10,7 @@ import {
 import { ListOptionMenu } from "./ListOptionMenu";
 import { CreateTaskForm } from "./CreateTaskForm";
 import Box from "@mui/material/Box";
-import useInfiniteScroll from "./InfiniteScroll";
+import { ListView } from "./ListView";
 
 export interface IFilterSortSetup {
   filter: FilterTypes;
@@ -37,7 +20,10 @@ export interface IFilterSortSetup {
   };
 }
 
-type TaskSimplified = Omit<Task, "deletedAt" | "createdAt" | "updatedAt">;
+export type TaskSimplified = Omit<
+  Task,
+  "deletedAt" | "createdAt" | "updatedAt"
+>;
 
 const FilterableTaskTable = () => {
   const [tasks, setTasks] = useState<TaskSimplified[]>([]);
@@ -50,21 +36,10 @@ const FilterableTaskTable = () => {
       }
     | undefined
   >();
-  const [hasMore, setHasMore] = useState<boolean>(false);
 
-  const [loaderRef, converge, setConverge] = useInfiniteScroll();
   const { state, dispatch } = useListOption();
 
   const prevPageRef = useRef<number>(1);
-
-  useEffect(() => {
-    if (converge && hasMore) {
-      dispatch({
-        type: "increment-page",
-      });
-    }
-    setConverge(false);
-  }, [converge, hasMore, dispatch, setConverge]);
 
   useEffect(() => {
     console.log(state);
@@ -100,118 +75,14 @@ const FilterableTaskTable = () => {
       .catch((err) => console.log(err));
   }, [state]);
 
-  useEffect(() => {
-    setHasMore(state.page < taskListProps?.maxPage);
-  }, [state.page, taskListProps?.maxPage]);
-
-  const toggleTaskCompletion = (id: string) => {
-    const task = tasks.find((t) => t.id === id);
-    let completionToggler = completeTask;
-    if (task.isCompleted) {
-      completionToggler = uncompleteTask;
-    }
-    completionToggler(id).then(() => {
-      setTasks(
-        tasks.map((task) => {
-          if (task.id === id) {
-            return { ...task, isCompleted: !task.isCompleted };
-          }
-          return task;
-        })
-      );
-    });
-  };
-
-  const removeTask = (id: string) => {
-    deleteTask(id)
-      .then(() => {
-        setTasks(tasks.filter((task) => task.id !== id));
-      })
-      .catch((err) => console.log(err));
-  };
-
   return (
     <Box>
       <ListOptionMenu />
-      <Box
-        sx={{
-          maxWidth: "650px",
-          margin: "0 auto",
-          height: "80vh",
-          overflow: "scroll",
-          border: "2px solid #f5f5f5",
-          borderRadius: "8px",
-        }}
-      >
-        <Box>
-          <TableContainer component={Paper}>
-            <Table aria-label="Task Lists">
-              <TableBody>
-                {tasks.map((task) => (
-                  <TableRow
-                    key={task.id}
-                    sx={{
-                      ...(task.isCompleted
-                        ? {
-                            backgroundColor: "#f5f5f5",
-                          }
-                        : {}),
-                    }}
-                    onClick={() => toggleTaskCompletion(task?.id)}
-                  >
-                    <TableCell
-                      sx={{
-                        ...(task.isCompleted
-                          ? {
-                              textDecoration: "line-through",
-                              color: "gray",
-                            }
-                          : {}),
-                      }}
-                    >
-                      {task.description}
-                    </TableCell>
-                    <TableCell
-                      align="right"
-                      sx={{
-                        width: "168px",
-                      }}
-                    >
-                      {task.priority === Priority.HIGH ? (
-                        <Chip
-                          label="High"
-                          size="small"
-                          color="error"
-                          variant="outlined"
-                        />
-                      ) : task.priority === Priority.LOW ? (
-                        <Chip label="Low" size="small" variant="outlined" />
-                      ) : (
-                        ""
-                      )}
-                      <IconButton
-                        type="button"
-                        sx={{ p: "10px" }}
-                        aria-label="delete task"
-                        onClick={() => removeTask(task.id)}
-                      >
-                        <DeleteOutline />
-                      </IconButton>
-                      <Checkbox
-                        inputProps={{ "aria-label": "Task Checkbox" }}
-                        id={`checkbox-${task.id}`}
-                        checked={task.isCompleted}
-                        onChange={() => toggleTaskCompletion(task?.id)}
-                      />
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
-        <Box ref={loaderRef}>{hasMore && <div>Loading...</div>}</Box>
-      </Box>
+      <ListView
+        tasks={tasks}
+        setTasks={setTasks}
+        maxPage={taskListProps?.maxPage || 1}
+      />
       <CreateTaskForm
         successFunction={() => {
           setTasks([]);
---
+++ frontend/src/components/ListView.tsx
@@ -0,0 +1,157 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+import Chip from "@mui/material/Chip";
+import Box from "@mui/material/Box";
+import DeleteOutline from "@mui/icons-material/DeleteOutline";
+
+import { Priority } from "../types/tasks";
+import { TaskSimplified } from "./FiterableTaskTable";
+import { completeTask, deleteTask, uncompleteTask } from "../api/tasks";
+import { Dispatch, useEffect, useState } from "react";
+import useInfiniteScroll from "./InfiniteScroll";
+import { useListOption } from "../context/ListOptionContext";
+
+export const ListView = ({
+  tasks,
+  setTasks,
+  maxPage,
+}: {
+  tasks: TaskSimplified[];
+  setTasks: Dispatch<TaskSimplified[]>;
+  maxPage: number;
+}) => {
+  const [hasMore, setHasMore] = useState<boolean>(false);
+  const [loaderRef, converge, setConverge] = useInfiniteScroll();
+
+  const { state, dispatch } = useListOption();
+
+  useEffect(() => {
+    if (converge && hasMore) {
+      dispatch({
+        type: "increment-page",
+      });
+    }
+    setConverge(false);
+  }, [converge, hasMore, dispatch, setConverge]);
+
+  useEffect(() => {
+    setHasMore(state.page < maxPage);
+  }, [state.page, maxPage]);
+
+  const toggleTaskCompletion = (id: string) => {
+    const task = tasks.find((t) => t.id === id);
+    let completionToggler = completeTask;
+    if (task.isCompleted) {
+      completionToggler = uncompleteTask;
+    }
+    completionToggler(id).then(() => {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === id) {
+            return { ...task, isCompleted: !task.isCompleted };
+          }
+          return task;
+        })
+      );
+    });
+  };
+
+  const removeTask = (id: string) => {
+    deleteTask(id)
+      .then(() => {
+        setTasks(tasks.filter((task) => task.id !== id));
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <Box
+      sx={{
+        maxWidth: "650px",
+        margin: "0 auto",
+        height: "80vh",
+        overflow: "scroll",
+        border: "2px solid #f5f5f5",
+        borderRadius: "8px",
+      }}
+    >
+      <Box>
+        <TableContainer component={Paper}>
+          <Table aria-label="Task Lists">
+            <TableBody>
+              {tasks.map((task) => (
+                <TableRow
+                  key={task.id}
+                  sx={{
+                    ...(task.isCompleted
+                      ? {
+                          backgroundColor: "#f5f5f5",
+                        }
+                      : {}),
+                  }}
+                  onClick={() => toggleTaskCompletion(task?.id)}
+                >
+                  <TableCell
+                    sx={{
+                      ...(task.isCompleted
+                        ? {
+                            textDecoration: "line-through",
+                            color: "gray",
+                          }
+                        : {}),
+                    }}
+                  >
+                    {task.description}
+                  </TableCell>
+                  <TableCell
+                    align="right"
+                    sx={{
+                      width: "168px",
+                    }}
+                  >
+                    {task.priority === Priority.HIGH ? (
+                      <Chip
+                        label="High"
+                        size="small"
+                        color="error"
+                        variant="outlined"
+                      />
+                    ) : task.priority === Priority.LOW ? (
+                      <Chip label="Low" size="small" variant="outlined" />
+                    ) : (
+                      ""
+                    )}
+                    <IconButton
+                      type="button"
+                      sx={{ p: "10px" }}
+                      aria-label="delete task"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        removeTask(task.id);
+                      }}
+                    >
+                      <DeleteOutline />
+                    </IconButton>
+                    <Checkbox
+                      inputProps={{ "aria-label": "Task Checkbox" }}
+                      id={`checkbox-${task.id}`}
+                      checked={task.isCompleted}
+                      onChange={() => toggleTaskCompletion(task?.id)}
+                      onClick={(e) => e.stopPropagation()}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+      <Box ref={loaderRef}>{hasMore && <div>Loading...</div>}</Box>
+    </Box>
+  );
+};
